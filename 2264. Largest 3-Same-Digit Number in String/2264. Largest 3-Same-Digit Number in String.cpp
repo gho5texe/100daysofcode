4edfaa3717c76a68119f368545788b4b787ab314
Pradyumn singh Padiyar <91543816+gho5texe@users.noreply.github.com>
2023-12-04 23:40:56 +0530

    Add files via upload

---
+++ 2264. Largest 3-Same-Digit Number in String/2264. Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    string largestGoodInteger(string& num) {
+        int count=0;
+        char prev='X', maxd=' ';
+        for(char c: num){
+            if (c==prev) count++;
+            else count=1;
+            if (count==3)
+                maxd=max(maxd, c);
+            prev=move(c);
+        }
+        return (maxd==' ')?"":string(3, maxd);
+    }
+};
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
